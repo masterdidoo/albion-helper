6a1395758749d899dc007826774864a0541ae590
AS <975396+masterdidoo@users.noreply.github.com>
2019-09-23 12:35:25 +0400

    control

---
+++ Albion.GUI/TestData.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Albion.Db.Items;
+
+namespace Albion.GUI
+{
+    public class TestData : List<SimpleItem>
+    {
+        public TestData()
+        {
+            CraftingRequirement[] empty = new CraftingRequirement[0];
+
+            SimpleItem item1 = new SimpleItem("test00", null)
+            {
+                CraftingRequirements = empty
+            };
+            Add(item1);
+
+            Add(new SimpleItem("test", null)
+            {
+                CraftingRequirements = new CraftingRequirement[]
+                {
+                    new CraftingRequirement()
+                    {
+                        CraftResources = new CraftResource[]
+                        {
+                            new CraftResource()
+                            {
+                                Count = 10,
+                                Item = item1
+                            }, 
+                        }
+                    }
+                }
+            });
+            Add(new SimpleItem("test2", null)
+            {
+                CraftingRequirements = empty
+            });
+            Add(new SimpleItem("test3", null)
+            {
+                CraftingRequirements = empty
+            });
+        }
+    }
+}
