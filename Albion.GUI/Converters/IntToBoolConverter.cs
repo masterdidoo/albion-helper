0192f4d81973574ead4b5006c9c4b329535e2c0a
AS <975396+masterdidoo@users.noreply.github.com>
2019-12-05 16:18:13 +0400

    liting

--- Albion.GUI/Converters/IntToBoolConverter.cs
+++ Albion.GUI/Converters/IntToBoolConverter.cs
@@ -4,24 +4,17 @@ using System.Windows.Data;
 
 namespace Albion.GUI.Converters
 {
-    public class PriceConverter : IValueConverter
+    public class IntToBoolConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (!(value is long)) return null;
-
-            var vr = (long) value / 10000;
-
-            return vr;
+            if ((value is string s) && long.TryParse(s, NumberStyles.Any, culture.NumberFormat, out var vr)) return vr >= 0;
+            return null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (!(value is string)) return null;
-
-            if (!long.TryParse((string) value, out var vr)) return null;
-
-            return vr * 10000;
+            throw new NotImplementedException();
         }
     }
 }
