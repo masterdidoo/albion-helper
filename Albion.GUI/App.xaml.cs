174dbfe94db4446e084279d4de34a8d38893bc34
AS <975396+masterdidoo@users.noreply.github.com>
2019-12-09 11:37:03 +0400

    add log

--- Albion.GUI/App.xaml.cs
+++ Albion.GUI/App.xaml.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using Albion.GUI.ViewModels;
+using NLog;
 
 namespace Albion.GUI
 {
@@ -14,10 +16,12 @@ namespace Albion.GUI
     /// </summary>
     public partial class App : Application
     {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
         private MainViewModel _vm;
 
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
+            _log.Info("Start");
             _vm = new MainViewModel();
             var v = new MainWindow(){DataContext = _vm};
             v.Show();
@@ -26,6 +30,13 @@ namespace Albion.GUI
         private void App_OnExit(object sender, ExitEventArgs e)
         {
             _vm.Dispose();
+            _log.Info("Stop");
+        }
+
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            _log.Error(e.Exception, "UnhandledException");
+            e.Handled = true;
         }
     }
 }
